d223533ceffe16685283a122384e340a3061fb7a
Bor Kae Hwang <borkaehw@lucidchart.com>
2018-10-16 16:09:12 -0600

    future scala

--- src/main/scala/ScalaBinary.scala
+++ src/main/scala/ScalaBinary.scala
@@ -1,5 +1,10 @@
 package foo
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Success
+import scala.util.Failure
+
 object ScalaBinary {
   def main(args: Array[String]) {
     println("Hello, world1")
@@ -8,7 +13,35 @@ object ScalaBinary {
 
     val testOne = new TestClassOne
     testOne.print
+
+    
+
     val testTwo = new TestClassTwo
     testTwo.print
+
+    val rst1 = futureTest("kevin")
+    val rst2 = futureTest("james")
+    val rst3 = futureTest("jared")
+  }
+
+  def returnMultiple(name: String): (String, Int, String) = {
+  	(name, 428, "umich")
+  }
+
+  def futureTest(name: String) {
+  	val f = Future {
+    	val (testName, testNum, testSchool) = returnMultiple(name)
+    	(testName, testNum, testSchool)
+    }
+
+  	f.onComplete {
+    	case Success(n) => {
+    		println("yes")
+    		println(n._1)
+    	}
+    	case Failure(ex) => {
+    		println("no")
+    	}
+    }
   }
 }
